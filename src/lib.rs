e26466ad8b4170446c16eee34403922020a9a4ef
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-01-03 17:49:14 +1100

    First passing test case

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
-#[test]
-fn it_works() {
+use std::path::PathBuf;
+use std::fs;
+
+
+pub enum ResultFormat {
+    Flat,
+    Nested,
+}
+
+pub struct DirectoryScanner {
+    root_dir: PathBuf,
+}
+
+impl DirectoryScanner {
+
+    pub fn new(root_dir: PathBuf, result_format: ResultFormat) -> DirectoryScanner {
+        DirectoryScanner { root_dir: root_dir }
+    }
+
+    pub fn scan(&mut self) -> Vec<String> {
+        let mut filepaths = vec![];
+        match fs::read_dir(&self.root_dir) {
+            Ok(read_dir) => {
+                for entry in read_dir {
+                    match entry {
+                        Ok(entry) => {
+                            let filetype = entry.file_type().unwrap();
+                            if filetype.is_file() {
+                                filepaths.push(entry.path().to_str().unwrap().to_string());
+                            }
+                        }
+                        Err(_) => {  }
+                    }
+                }
+            }
+            Err(_) => { } // this should never happen what do we do just in case?
+        }
+        filepaths
+    }
 }
---
+++ tests/lib.rs
@@ -0,0 +1,55 @@
+#![feature(plugin,const_fn)]
+#![plugin(stainless)]
+
+extern crate directory_scanner;
+
+#[cfg(test)]
+mod tests {
+    pub use directory_scanner::DirectoryScanner;
+    pub use directory_scanner::ResultFormat;
+    pub use std::path::PathBuf;
+
+    describe! stainless {
+
+        before_each {
+            let path = PathBuf::from("./tests/fixtures/dir-with-11-files/");
+        }
+
+        describe! flat_format {
+
+            before_each {
+                let result_format = ResultFormat::Flat;
+                let mut directory_scanner = DirectoryScanner::new(path, result_format);
+                let results = directory_scanner.scan();
+            }
+
+            it "returns finds all the files" {
+                assert_eq!(results.len(), 11);
+            }
+
+            //it "returns the results as a list of strings" {
+            //assert_eq!(results[0], "first-file");
+            //assert_eq!(results[-1], "last-file");
+            //}
+        }
+
+        //describe! nested_format {
+
+        //before_each {
+        //let result_format = ResultFormat::Flat;
+        //let mut directory_scanner = DirectoryScanner::new(result_format);
+        //let results = directory_scanner.scan(path);
+        //}
+
+        //it "returns finds all the files" {
+        //assert_eq!(len(results), 11);
+        //}
+
+        //it "returns the results as a list of strings" {
+        //assert_eq!(results[0], "first-file");
+        //assert_eq!(results[-1], "last-file");
+        //}
+        //}
+
+    }
+}
